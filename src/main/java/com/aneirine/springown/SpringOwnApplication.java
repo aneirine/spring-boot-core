bf90f1781e3cbaf6afa160e9fc25dde7d3f132cc
Aneirine <anyashi2002@gmail.com>
2020-12-05 20:13:10 +0200

    Init

---
+++ src/main/java/com/aneirine/springown/SpringOwnApplication.java
@@ -0,0 +1,13 @@
+package com.aneirine.springown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOwnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOwnApplication.class, args);
+    }
+
+}
