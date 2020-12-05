7a03115b40c628fc981934f94fff60cb7f83c9fe
Aneirine <anyashi2002@gmail.com>
2020-12-05 20:45:34 +0200

    Add shema to data fields

--- src/main/java/com/aneirine/springown/modules/domain/UserData.java
+++ src/main/java/com/aneirine/springown/modules/domain/UserData.java
@@ -1,5 +1,6 @@
 package com.aneirine.springown.modules.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Setter
 public class UserData {
 
+    @Schema(description = "Login for user", example = "aneirine", required = true)
     private String login;
+    @Schema(description = "Password for user", example = "qwerty123!", required = true)
     private String password;
 }
