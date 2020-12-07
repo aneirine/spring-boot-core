3cae3270e32dc7d8dcd24cb3f96a2d76176c5306
Aneirine <anuashi2002@gmail.com>
2020-12-07 19:21:01 +0200

    Add roles, add ant matchers for roles

--- open-api/src/main/java/com/aneirine/springown/config/SecurityConfig.java
+++ open-api/src/main/java/com/aneirine/springown/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.aneirine.springown.config;
 
 
+import com.aneirine.springown.entities.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,7 +21,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+
+
+                .antMatchers(HttpMethod.GET, "/api/**")
+                .hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+
+                .antMatchers(HttpMethod.POST, "/api/**")
+                .hasAnyRole(Role.ADMIN.name())
+
+                .antMatchers(HttpMethod.DELETE, "/api/**")
+                .hasAnyRole(Role.ADMIN.name())
+
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
@@ -29,7 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("aneirine")
                         .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN")
+                        .roles(Role.ADMIN.name())
+                        .build(),
+
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles(Role.USER.name())
                         .build()
         );
     }
